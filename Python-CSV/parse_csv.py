60ba711ba297f83d7be60b26a10f65c3fc75dc4f
amrugal <52331409+amrugal@users.noreply.github.com>
2019-12-01 20:03:18 +0100

    Update parse_csv.py

--- Python-CSV/parse_csv.py
+++ Python-CSV/parse_csv.py
@@ -2,8 +2,7 @@ import csv
 
 with open('names.csv', 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
-    # next() function ommits first row(with keys) in csv file like ['first_name', 'last_name', 'email']
-    # next(csv_reader)
+    
     with open('new_names.csv', 'w') as new_file:
         fieldnames = ['first_name', 'last_name']
 
@@ -14,5 +13,4 @@ with open('names.csv', 'r') as csv_file:
         for line in csv_reader:
             del line['email']
             csv_writer.writerow(line)
-            # to print out only first_name values we can use indexing [0]
-            # print(line[0])
+            
